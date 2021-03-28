a144b8dbbd266812c46da032f975f6a5568c6466
Misha Brukman <mbrukman@google.com>
2021-03-28 14:36:53 -0400

    Add Go 1.16 tests and disable `go mod tidy` test
    Note: we're only disabling the `go mod tidy` test when running under Go 1.16,
    since its output differs from all earlier Go versions we are currently
    supporting. The test is still enabled for all other versions.

--- go_mod_tidy_test.sh
+++ go_mod_tidy_test.sh
@@ -16,6 +16,20 @@
 
 # Verifies that we have run `go mod tidy` to keep our modules config clean.
 
+# Go 1.16 seems to differ in how it formats `go mod` using `go mod tidy`, which
+# makes it incompatible with all prior versions; see the output logs in
+# https://github.com/google/code-review-bot/actions/runs/695615181 for the
+# differences in results. Thus, for now, we'll skip this test for Go 1.16 so that:
+#
+# (a) we can continue to test all other functionality with Go 1.16
+# (b) users using Go 1.16 can run `make test` just like everyone else
+if [ -n "$(go version | grep 'go1.16')" ]; then
+  echo "WARNING: Go 1.16 uses a different format for 'go mod tidy' output." >&2
+  echo "WARNING: See https://github.com/google/code-review-bot/actions/runs/695615181 for details." >&2
+  echo "WARNING: Skipping $(basename $0) for now." >&2
+  exit
+fi
+
 declare -r GO_MOD="go.mod"
 declare -r GO_SUM="go.sum"
 
